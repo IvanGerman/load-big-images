fa2782a4f09f2e2407161fa2e8b7b486dc2aa573
IvanGerman <ivgvi@mail.ru>
2023-03-17 14:25:53 +0700

    feat: add blur effect to the image

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log(image.src);
 const bigImage = document.createElement('img');
 bigImage.onload = function() {
   image.src = this.src;
-  console.log(image.src);  
+  image.className = 'noblur';  
 };
 
 bigImage.src = 'http://127.0.0.1:5500/assets/images/bg2-big.jpg';
